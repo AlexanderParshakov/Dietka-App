d90a6d1ab654e59f74e38d43d3b0c55f67015e60
AlexanderParshakov <alls.parshakov@gmail.com>
2020-11-01 21:31:42 +0300

    Initial Commit

---
+++ Dietka App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dietka App
+//
+//  Created by Parshakov Alexander on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
